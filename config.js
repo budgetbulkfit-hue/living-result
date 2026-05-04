ada25f6c2980b393a60a30c08c5e44d590b606c4
Living Result <budgetbulkfit@gmail.com>
2026-05-04 23:11:47 +0530

    google sheet app script

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 (function () {
   const defaults = {
     apiUrl: 'https://living-result-backend.onrender.com/api',
-    googleWebAppUrl: '',
+    googleWebAppUrl: 'https://script.google.com/macros/s/AKfycbwWTolkQqA0LXgLwTYj8vnWMoEHQeonlhCc7-8RDEXgnGzZG6C22wK_RInl6Gkh0t3o8A/exec', // <-- PASTE YOUR URL HERE
   };
 
   const injectedConfig = window.__APP_CONFIG__ || {};
