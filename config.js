ce177c306ab2cf1ba05386bfca620d6a8ba654ac
Living Result <budgetbulkfit@gmail.com>
2026-05-05 23:34:04 +0530

    admin page update

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 (function () {
+  const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
   const defaults = {
-    apiUrl: 'http://localhost:5000/api',
+    apiUrl: isLocal ? 'http://localhost:5000/api' : 'https://living-result-backend.onrender.com/api',
     googleWebAppUrl: 'https://script.google.com/macros/s/AKfycbwWTolkQqA0LXgLwTYj8vnWMoEHQeonlhCc7-8RDEXgnGzZG6C22wK_RInl6Gkh0t3o8A/exec', // <-- PASTE YOUR URL HERE
   };
 
